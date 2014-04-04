6b6ddf4ed74c63d9a3d17cf79edb365c4f45619c
John Cech & Mark Lehman <student@epicodus.com>
2014-04-04 13:43:09 -0700

    edit2 production environment for S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,12 +30,14 @@ Ecard::Application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/convert"
 
   # config/environments/production.rb
-  config.paperclip_defaults = {
+  config.paperclip_defaults.merge!({
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+    },
+    url: ":s3_domain_url",
+    path: "/:class/:attachment/:id_partition/:style/:filename"
+  })
 end
