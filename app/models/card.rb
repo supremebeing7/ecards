f9d7fffc7bd4d4bbe66fe2d86465e4144a0bd696
John Cech & Mark Lehman <student@epicodus.com>
2014-04-04 14:01:17 -0700

    add bucket and storage to card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,10 @@ class Card < ActiveRecord::Base
   belongs_to :category
   has_and_belongs_to_many :ratings
 
-  has_attached_file :card_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :card_image,
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png",
+                    :storage => :s3,
+                    :bucket  => ENV['S3_BUCKET_NAME']
   validates_attachment_content_type :card_image, :content_type => /\Aimage\/.*\Z/
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,14 +30,16 @@ Ecard::Application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/convert"
 
   # config/environments/production.rb
-  config.paperclip_defaults.merge!({
+  config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    },
-    url: ":s3_domain_url",
-    path: "/:class/:attachment/:id_partition/:style/:filename"
-  })
+    }
+  }
+
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
 end
